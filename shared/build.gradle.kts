948445872fc188680960586e2c5ea118e6c519ac
Gowtham Shanmugaraj GANESAN <gowthamshanmugaraj.g@nishtech.be>
2025-03-25 15:21:00 +0100

    Add required dependencies to the project including Compose

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -28,6 +28,25 @@ kotlin {
     }
 
     sourceSets {
+
+        // Old way of defining commonMain and commonTest
+        val commonMain by getting {
+            dependencies {
+                implementation("com.russhwolf:multiplatform-settings:1.3.0")
+                implementation("com.russhwolf:multiplatform-settings-no-arg:1.3.0")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
+                // https://mvnrepository.com/artifact/org.jetbrains.kotlinx/kotlinx-coroutines-core
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
+                implementation("io.insert-koin:koin-core:4.0.2")
+
+                // Compose
+                implementation("org.jetbrains.compose.runtime:runtime:1.7.3")
+                implementation("org.jetbrains.compose.foundation:foundation:1.7.3")
+                implementation("org.jetbrains.compose.material:material:1.7.3")
+            }
+        }
+
+        // New way of defining commonMain and commonTest
         commonMain.dependencies {
             //put your multiplatform dependencies here
         }
